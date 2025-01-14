f8c3a9c91b3e517500f2c919cb596a7381d4d5ef
Taimoor <taimoorshafique541@gmail.com>
2025-01-14 20:41:18 +0500

    update: y-space

--- components/portofolio-page.tsx
+++ components/portofolio-page.tsx
@@ -92,7 +92,7 @@ function Portfolio() {
         <div className="flex-col flex items-center justify-center bg-black text-white min-h-screen p-8 pb-20 gap-16 sm:p-20 w-full">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
                 <div className="flex flex-col">
-                    <div className="flex gap-4 justify-center items-center">
+                    <div className="flex gap-4 justify-center items-center mb-8">
                         <button className="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
                             <span>
                                 <span className="spark mask-gradient absolute inset-0 h-full w-full animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
